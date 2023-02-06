f1b6d3e551a7a014c0a2f6764216493eb240dd98
Bartol Deak <b@bdeak.net>
2023-02-06 03:04:30 +0100

    store parity in hex and distribute it across the blocks

--- seed-parity.js
+++ seed-parity.js
@@ -1,4 +1,4 @@
-const stripWithDistributedParity = (data, blockCount, parityCount) => {
+function stripWithDistributedParity(data, blockCount, parityCount) {
   if (parityCount > 0 && blockCount - parityCount < 2) {
     throw new Error("not enough blocks to strip with parity");
   }
@@ -10,49 +10,44 @@ const stripWithDistributedParity = (data, blockCount, parityCount) => {
   const dataCount = blockCount - parityCount;
   const blockLength = Math.ceil(data.length / dataCount);
 
+  let parityIndex = 0;
   for (let i = 0; i < blockLength; i++) {
-    const dataRow = data.slice(i * dataCount, (i + 1) * dataCount);
-    while (dataRow.length < dataCount) {
-      dataRow.push(dataRow[dataRow.length - 1]);
+    const row = data.slice(i * dataCount, (i + 1) * dataCount);
+    while (row.length < dataCount) {
+      row.push(row[row.length - 1]);
     }
 
-    const dataRowBin = dataRow.map(convertBin);
-    console.log(dataRow, [...dataRowBin, xor(dataRowBin)]);
+    const parity = "0x" + bin2hex(xor(row.map(str2bin)));
+    for (let j = 0; j < parityCount; j++) {
+      row.splice(parityIndex++ % blockCount, 0, parity);
+    }
+
+    for (let j = 0; j < blockCount; j++) {
+      blocks[j].push(row[j]);
+    }
   }
 
-  // let splitIndex = 0;
-  // for (i = 0; i < words.length; i++) {
-  //   const split = splits[splitIndex++ % splitCount];
-  //   split[i] = "null";
-  // }
+  return blocks.map((block) => block.join(" "));
+}
 
-  // console.log(i % splitCount);
-  // splits[i % splitCount] = "null";
-  // i++;
-  // i = (i + 1) % splitCount;
-
-  // for (const word of words) {
-  //   for (const split of splits) {
-  //     split.push(word);
-  //     console.log(word, split);
-  //   }
-  //   //   seedSplits[0].push(word);
-  // }
-
-  // console.table(blocks);
-};
-
-function convertBin(str) {
+function str2bin(str) {
   return str
     .split("")
     .map((x) => x.charCodeAt(0).toString(2).padStart(8, "0"))
     .join(" ");
 }
 
-function convertStr(bin) {
+function bin2str(bin) {
   return bin
     .split(" ")
     .map((x) => String.fromCharCode(parseInt(x, 2)))
+    .join("");
+}
+
+function bin2hex(bin) {
+  return bin
+    .split(" ")
+    .map((x) => parseInt(x, 2).toString(16).toUpperCase())
     .join("");
 }
 
@@ -71,26 +66,12 @@ function xor(strArr) {
   return parity;
 }
 
-const _str = (bin) => {
-  return bin.replace(/\s*[01]{8}\s*/g, (bin) =>
-    String.fromCharCode(parseInt(bin, 2))
-  );
-};
-
-stripWithDistributedParity(
-  "testiranjee test1 test2 test3 test4 test5 test6 test7 test8 test9".split(
-    " "
-  ),
-  4,
-  1
-);
-
-console.log(
-  convertStr(
-    xor([
-      "01110100 01100101 01110011 01110100 00110010",
-      "01110100 01100101 01110011 01110100 01101001 01110010 01100001 01101110 01101010 01100101 01100110",
-      "01110100 01100101 01110011 01110100 01101001 01110010 01100001 01101110 01101010 01100101 01100101",
-    ])
-  )
+console.table(
+  stripWithDistributedParity(
+    "testiranjee test1 test2 test3 test4 test5 test6 test7 test8 test9".split(
+      " "
+    ),
+    4,
+    1
+  ).map((x) => x.split(" "))
 );
