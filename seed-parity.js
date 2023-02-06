d3abc760cfffdc5c5a1821831575d94fac7ffdfb
Bartol Deak <b@bdeak.net>
2023-02-06 11:44:10 +0100

    split different parity configurations in separate configurations

--- seed-parity.js
+++ seed-parity.js
@@ -1,13 +1,15 @@
-function stripWithDistributedParity(data, blockCount, parityCount) {
-  if (parityCount > 0 && blockCount - parityCount < 2) {
-    throw new Error("not enough blocks to strip with parity");
+function strip(data, blockCount) {}
+
+function stripWithDistributedParity(data, blockCount) {
+  if (blockCount < 3) {
+    throw new Error("not enough blocks to strip with distributed parity");
   }
 
   const blocks = Array(blockCount)
     .fill(null)
     .map(() => []);
 
-  const dataCount = blockCount - parityCount;
+  const dataCount = blockCount - 1;
   const blockLength = Math.ceil(data.length / dataCount);
 
   let parityIndex = 0;
@@ -18,17 +20,35 @@ function stripWithDistributedParity(data, blockCount, parityCount) {
     }
 
     const parity = "0x" + bin2hex(xor(row.map(str2bin)));
-    for (let j = 0; j < parityCount; j++) {
-      row.splice(parityIndex++ % blockCount, 0, parity);
-    }
+    row.splice(parityIndex++ % blockCount, 0, parity);
 
     for (let j = 0; j < blockCount; j++) {
       blocks[j].push(row[j]);
     }
   }
 
-  return blocks.map((block) => block.join(" "));
+  return blocks;
 }
+
+function stripWithDualDistributedParity(data, blockCount) {}
+
+function rebuild(blocks) {}
+
+function rebuildWithDistributedParity(blocks) {
+  const data = [];
+  for (let i = 0; i < blocks[0].length; i++) {
+    const row = blocks.map((x) => x[i]);
+    const x2bin = (x) => (x.startsWith("0x") ? hex2bin : str2bin)(x);
+    const parity = xor(row.map(x2bin));
+    console.log(parity);
+    console.log(str2bin("otorionralingologija"));
+    console.log(bin2str(parity));
+  }
+  console.log(blocks);
+  return data;
+}
+
+function rebuildWithDualDistributedParity(blocks) {}
 
 function str2bin(str) {
   return str
@@ -51,12 +71,22 @@ function bin2hex(bin) {
     .join("");
 }
 
+function hex2bin(hex) {
+  return hex
+    .replace("0x", "")
+    .match(/.{1,2}/g)
+    .map((x) => parseInt(x, 16).toString(2).padStart(8, "0"))
+    .join(" ");
+}
+
 function xor(strArr) {
-  const maxLength = Math.max(...strArr.map((x) => x.length));
-  const zeroPadded = strArr.map((x) => x.padStart(maxLength, "0"));
+  const maxLength = Math.max(...strArr.map((x) => x.split(" ").length));
+  const zeroPad = (length) => "00000000 ".repeat(maxLength - length);
+  const zeroPadded = strArr.map((x) => zeroPad(x.split(" ").length) + x);
+  console.log(zeroPadded);
 
   let parity = "";
-  for (let i = 0; i < maxLength; i++) {
+  for (let i = 0; i < zeroPadded[0].length; i++) {
     if (zeroPadded[0][i] === " ") {
       parity += " ";
       continue;
@@ -67,11 +97,12 @@ function xor(strArr) {
 }
 
 console.table(
-  stripWithDistributedParity(
-    "testiranjee test1 test2 test3 test4 test5 test6 test7 test8 test9".split(
-      " "
-    ),
-    4,
-    1
-  ).map((x) => x.split(" "))
+  rebuildWithDistributedParity(
+    stripWithDistributedParity(
+      "otorionralingologija test1 test2 test3 test4 test5 test6 test7 test8 test9".split(
+        " "
+      ),
+      4
+    ).slice(1)
+  )
 );
